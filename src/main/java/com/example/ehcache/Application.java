038d124dc40ed815edbd0fdbd084bd7ce05ec79a
janithasen@gmail.com <janithasen@gmail.com>
2018-04-13 13:24:23 +0530

    changed the dependencies to avoid incompatibility errors

--- src/main/java/com/example/ehcache/Application.java
+++ src/main/java/com/example/ehcache/Application.java
@@ -2,16 +2,13 @@ package com.example.ehcache;
 
 import com.example.ehcache.entities.Book;
 import com.example.ehcache.repositories.BookRepository;
-//import org.hibernate.cache.redis.SingletonRedisRegionFactory;
-import org.hibernate.cfg.Environment;
+import org.hibernate.cache.redis.hibernate5.SingletonRedisRegionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.ComponentScan;
 
-//import java.util.List;
 import java.util.Optional;
 
 @SpringBootApplication
@@ -49,7 +46,7 @@ public class Application implements CommandLineRunner {
             Optional<Book> book = bookRepository.findById(i);
             System.out.println(book.get());
             Thread.sleep(2000);
-           // SingletonRedisRegionFactory k;
+           SingletonRedisRegionFactory k;
         }
     }
 }
